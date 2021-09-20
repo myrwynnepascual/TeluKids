f1dfde6be0cf69a9cca1c5f72d9545684acd074e
ReinLopez <reinhardtheinz.lopez.iics@ust.edu.ph>
2021-09-20 20:00:15 +0800

    with Discipline Quiz

---
+++ app/src/main/java/com/example/telukidsv1/QuizDiscipline.java
@@ -0,0 +1,241 @@
+package com.example.telukidsv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class QuizDiscipline extends AppCompatActivity {
+    private TextView countLabel_Discipline;
+    private TextView questionLabel_Discipline;
+    private LinearLayout quizLayout_Discipline;
+    private ConstraintLayout background_Discipline;
+
+    MediaPlayer voiceover1;
+    MediaPlayer voiceover2;
+    MediaPlayer choice1;
+    MediaPlayer choice2;
+
+    int background_Question;
+
+    Button btnAnswer1_Discipline;
+    Button btnAnswer2_Discipline;
+    Button btnConfirm_Discipline;
+
+    private String rightAnswer_Discipline;
+    private int rightAnswerCount_Discipline = 0;
+    private int quizCount_Discipline = 1;
+    static final private int QUIZ_COUNT = 5;
+    private int confirmClicked_Discipline = 0;
+
+
+    ArrayList<ArrayList<String>> quizArray_Discipline = new ArrayList<>();
+
+    String quizData_Discipline[][] = {
+            //{"Question", "Background Image 1", "Question Voice Over 1", "Background Image 2", "Question Voice Over 2" "Right Answer", "Wrong Answer", "Choice 1 Voice Over", "Choice 2 Voice Over"}
+            {"What should Joey do?", String.valueOf(R.drawable.d1bg1), String.valueOf(R.raw.dq1_1), String.valueOf(R.drawable.d1bg2), String.valueOf(R.raw.dq1_2), "Start coloring his drawing ", "Go around the classroom and talk to his seatmates", String.valueOf(R.raw.dq1c1), String.valueOf(R.raw.dq1c2) },
+            {"What should Joey do?", String.valueOf(R.drawable.d2bg1), String.valueOf(R.raw.dq2_1), String.valueOf(R.drawable.d2bg2), String.valueOf(R.raw.dq2_2), "Clean up his toys", "Leave his toys on the floor", String.valueOf(R.raw.dq2c1), String.valueOf(R.raw.dq2c2) },
+            {"What should Julie do?", String.valueOf(R.drawable.d3bg1), String.valueOf(R.raw.dq3_1), String.valueOf(R.drawable.d3bg2), String.valueOf(R.raw.dq3_2), "Patiently wait for her turn", "Become grumpy for waiting in line", String.valueOf(R.raw.dq3c1), String.valueOf(R.raw.dq3c2) },
+            {"What should Max do?", String.valueOf(R.drawable.d4bg1), String.valueOf(R.raw.dq4_1), String.valueOf(R.drawable.d4bg2), String.valueOf(R.raw.dq4_2), "Tell his parents the truth that he is sad because he failed his exam", "Keep his feelings to himself and tell his parents that everything is okay", String.valueOf(R.raw.dq4c1), String.valueOf(R.raw.dq4c2) },
+            {"What should Marga do?", String.valueOf(R.drawable.d5bg1), String.valueOf(R.raw.dq5_1), String.valueOf(R.drawable.d5bg2), String.valueOf(R.raw.dq5_2), "Pick up the piece of paper and throw it in the trash", "Ignore the piece of paper", String.valueOf(R.raw.dq5c1), String.valueOf(R.raw.dq5c2) },
+            {"What should Marga do?", String.valueOf(R.drawable.d6bg1), String.valueOf(R.raw.dq6_1), String.valueOf(R.drawable.d6bg2), String.valueOf(R.raw.dq6_2), "Tell her mom she wants to do it by herself and brushes her teeth", "Let her mom brush her teeth", String.valueOf(R.raw.dq6c1), String.valueOf(R.raw.dq6c2) }
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_discipline);
+
+        background_Discipline = (ConstraintLayout) findViewById(R.id.background_Discipline);
+        quizLayout_Discipline = (LinearLayout)findViewById(R.id.quizLayout_Discipline);
+        countLabel_Discipline = (TextView)findViewById(R.id.countLabel_Discipline);
+        questionLabel_Discipline = (TextView)findViewById(R.id.questionLabel_Discipline);
+        btnAnswer1_Discipline = (Button)findViewById(R.id.btnAnswer1_Discipline);
+        btnAnswer2_Discipline = (Button)findViewById(R.id.btnAnswer2_Discipline);
+        btnConfirm_Discipline = (Button)findViewById(R.id.btnConfirm_Discipline);
+
+
+        //Create quizArray from quizData
+        for(int i = 0; i < quizData_Discipline.length; i++){
+            ArrayList<String> tmpArray = new ArrayList<>();
+            tmpArray.add(quizData_Discipline[i][0]); //Question
+            tmpArray.add(quizData_Discipline[i][1]); //Background Image 1
+            tmpArray.add(quizData_Discipline[i][2]); //Question Voice Over 1
+            tmpArray.add(quizData_Discipline[i][3]); //Background Image 2
+            tmpArray.add(quizData_Discipline[i][4]); //Question Voice Over 2
+            tmpArray.add(quizData_Discipline[i][5]); //Right Answer
+            tmpArray.add(quizData_Discipline[i][6]); //Wrong Answer
+            tmpArray.add(quizData_Discipline[i][7]); //Choice 1 Voice Over
+            tmpArray.add(quizData_Discipline[i][8]); //Choice 2 Voice Over
+            //Add tmpArray to quizArray
+            quizArray_Discipline.add(tmpArray);
+        }
+        showNextQuiz();
+    }
+    public void showNextQuiz(){
+        //Update quizCountLabel
+        countLabel_Discipline.setText("Question #" + quizCount_Discipline);
+        confirmClicked_Discipline = 0;
+
+        //Generate random number
+        Random random = new Random();
+        int randomNum = random.nextInt(quizArray_Discipline.size());
+
+        //Pick one quiz set
+        ArrayList<String> quiz = quizArray_Discipline.get(randomNum);
+
+        //Set Question and Right Answer
+
+        //Hide question and choices
+        countLabel_Discipline.setVisibility(View.INVISIBLE);
+        questionLabel_Discipline.setVisibility(View.INVISIBLE);
+        btnAnswer1_Discipline.setVisibility(View.INVISIBLE);
+        btnAnswer2_Discipline.setVisibility(View.INVISIBLE);
+        btnConfirm_Discipline.setVisibility(View.INVISIBLE);
+
+        questionLabel_Discipline.setText(quiz.get(0));
+        background_Discipline.setBackgroundResource(Integer.parseInt(quiz.get(1)));
+        voiceover1 = MediaPlayer.create(this, Integer.parseInt(quiz.get(2)));
+        background_Question = Integer.parseInt(quiz.get(3));
+        voiceover2 = MediaPlayer.create(this, Integer.parseInt(quiz.get(4)));
+        voiceover1.start();
+        voiceover1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                voiceover2.start();
+                background_Discipline.setBackgroundResource(background_Question);
+                countLabel_Discipline.setVisibility(View.VISIBLE);
+                questionLabel_Discipline.setVisibility(View.VISIBLE);
+                btnAnswer1_Discipline.setVisibility(View.VISIBLE);
+                btnAnswer2_Discipline.setVisibility(View.VISIBLE);
+                btnConfirm_Discipline.setVisibility(View.VISIBLE);
+            }
+        });
+        rightAnswer_Discipline = quiz.get(5);
+        choice1 = MediaPlayer.create(this, Integer.parseInt(quiz.get(7)));
+        choice2 = MediaPlayer.create(this, Integer.parseInt(quiz.get(8)));
+
+        //Shuffle Choices
+        quiz.remove(0);
+        quiz.remove(0);
+        quiz.remove(0);
+        quiz.remove(0);
+        quiz.remove(0);
+        quiz.remove(2);
+        quiz.remove(2);
+        Collections.shuffle(quiz);
+
+        //Set Choices
+        btnAnswer1_Discipline.setText(quiz.get(0));
+        btnAnswer2_Discipline.setText(quiz.get(1));
+
+        //Remove this quiz from array
+        quizArray_Discipline.remove(randomNum);
+    }
+
+    public void checkAnswer(View view){
+        //Get pushed button
+        Button answerBtn = (Button) findViewById(view.getId());
+        String btnText = answerBtn.getText().toString();
+        MediaPlayer correct_sound = MediaPlayer.create(this, R.raw.correctsound);
+        MediaPlayer wrong_sound =  MediaPlayer.create(this, R.raw.wrongsound);
+
+        if (answerBtn == btnAnswer1_Discipline){
+            btnAnswer1_Discipline.setBackgroundResource(R.drawable.selectedanswerbutton);
+            btnAnswer2_Discipline.setBackgroundResource(R.drawable.answerbutton);
+        }
+        else if (answerBtn == btnAnswer2_Discipline){
+            btnAnswer2_Discipline.setBackgroundResource(R.drawable.selectedanswerbutton);
+            btnAnswer1_Discipline.setBackgroundResource(R.drawable.answerbutton);
+        }
+
+        if (answerBtn.getText().equals(rightAnswer_Discipline)){
+            voiceover2.pause();
+            choice1.start();
+        }
+        else if (!answerBtn.getText().equals(rightAnswer_Discipline)){
+            voiceover2.pause();
+            choice2.start();
+        }
+
+        // Confirm Users answer and shows if answer is right or wrong
+        btnConfirm_Discipline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                voiceover2.pause();
+                choice1.pause();
+                choice2.pause();
+                if(btnText.equals(rightAnswer_Discipline)){
+                    //Correct
+                    answerBtn.setBackgroundResource(R.drawable.correctanswerbutton);
+                    correct_sound.start();
+                    rightAnswerCount_Discipline++;
+                    btnConfirm_Discipline.setEnabled(false);
+                    btnAnswer1_Discipline.setEnabled(false);
+                    btnAnswer2_Discipline.setEnabled(false);
+                    confirmClicked_Discipline++;
+                }
+                else if (!btnText.equals(rightAnswer_Discipline) && answerBtn != btnConfirm_Discipline) {
+                    if (btnText.equals(btnAnswer1_Discipline.getText().toString()) || btnText.equals(btnAnswer2_Discipline.getText().toString()) ) {
+                        //Wrong
+                        answerBtn.setBackgroundResource(R.drawable.wronganswerbutton);
+                        wrong_sound.start();
+                        btnConfirm_Discipline.setEnabled(false);
+                        btnAnswer1_Discipline.setEnabled(false);
+                        btnAnswer2_Discipline.setEnabled(false);
+                        confirmClicked_Discipline++;
+                    } else{
+                        Toast.makeText(getApplicationContext(),"Please Select an Answer",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        quizLayout_Discipline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (quizCount_Discipline == QUIZ_COUNT && confirmClicked_Discipline != 0){
+                    //Show Result
+                    Intent intent = new Intent(getApplicationContext(), ResultsDiscipline.class);
+                    intent.putExtra("RIGHT_ANSWER_COUNT_Discipline", rightAnswerCount_Discipline);
+                    startActivity(intent);
+                }
+                else if (!btnText.equals(btnAnswer1_Discipline.getText().toString()) && !btnText.equals(btnAnswer2_Discipline.getText().toString())){
+                    //Check if user selected an answer
+                    Toast.makeText(getApplicationContext(),"Please Select an Answer",Toast.LENGTH_LONG).show();
+
+                }
+                else if(confirmClicked_Discipline == 0){
+                    //Check if Confirm Answer Button was clicked
+                    Toast.makeText(getApplicationContext(),"Please Confirm your Answer",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    quizCount_Discipline++;
+                    btnAnswer1_Discipline.setBackgroundResource(R.drawable.answerbutton);
+                    btnAnswer2_Discipline.setBackgroundResource(R.drawable.answerbutton);
+                    btnAnswer1_Discipline.setEnabled(true);
+                    btnAnswer2_Discipline.setEnabled(true);
+                    btnConfirm_Discipline.setEnabled(true);
+                    correct_sound.reset();
+                    wrong_sound.reset();
+                    showNextQuiz();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/telukidsv1/ResultsDiscipline.java
@@ -0,0 +1,97 @@
+
+package com.example.telukidsv1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Transaction;
+
+public class ResultsDiscipline extends AppCompatActivity {
+    private int  score_discipline;
+    MediaPlayer congrats;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    FirebaseUser user;
+    String achievement_discipline;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.results_discipline);
+        TextView totalScoreLabel = findViewById(R.id.totalScoreLabel_Discipline);
+        ImageView imgTrophy_Discipline = findViewById(R.id.imgTrophy_Discipline);
+        ImageButton achievementsbtnCLC_Discipline = findViewById(R.id.achievementsbtnCLC_Discipline);
+        ImageButton btnReadLesson_Quiz_Discipline = findViewById(R.id.btnReadLesson_Quiz_Discipline);
+        ImageButton homepageCLC_Discipline = findViewById(R.id.homepageCLC_Discipline);
+
+        congrats = MediaPlayer.create(this, R.raw.yaysfx);
+        congrats.start();
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        user = fAuth.getCurrentUser();
+        DocumentReference docRef = fStore.collection("users").document(user.getUid());
+
+        score_discipline = getIntent().getIntExtra("RIGHT_ANSWER_COUNT_Discipline", 0);
+        totalScoreLabel.setText(score_discipline + " / 5");
+
+        if (score_discipline == 0 || score_discipline == 1 || score_discipline == 2){
+            imgTrophy_Discipline.setImageResource(R.drawable.congratsbadgediscipline);
+            achievement_discipline = "Discipline Beginner";
+        }
+        if (score_discipline == 3 || score_discipline == 4){
+            imgTrophy_Discipline.setImageResource(R.drawable.congratsmedaldiscipline);
+            achievement_discipline = "Discipline Expert";
+        }
+        if (score_discipline == 5){
+            imgTrophy_Discipline.setImageResource(R.drawable.congratstrophydiscipline);
+            achievement_discipline = "Discipline Master";
+        }
+
+        fStore.runTransaction(new Transaction.Function<Void>() {
+            @Override
+            public Void apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
+                DocumentSnapshot snapshot = transaction.get(docRef);
+
+                transaction.update(docRef, "discipline quiz score", score_discipline);
+                transaction.update(docRef, "discipline achievement", achievement_discipline);
+                return null;
+            }
+        });
+
+        achievementsbtnCLC_Discipline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent proceed = new Intent(ResultsDiscipline.this, Achievements3to6Main.class);
+                startActivity(proceed);
+            }
+        });
+        btnReadLesson_Quiz_Discipline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent proceed = new Intent(ResultsDiscipline.this, Gmrc3to6.class);
+                startActivity(proceed);
+            }
+        });
+        homepageCLC_Discipline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent proceed = new Intent(ResultsDiscipline.this, Homepage3to6.class);
+                startActivity(proceed);
+            }
+        });
+    }
+}
