2b352363649f50ebf118bb786259da24a690b0d9
ReinLopez <reinhardtheinz.lopez.iics@ust.edu.ph>
2021-10-07 22:57:23 +0800

    updated Sign Up

--- app/src/main/java/com/example/telukidsv1/SuccessfulSignUp.java
+++ app/src/main/java/com/example/telukidsv1/SuccessfulSignUp.java
@@ -15,18 +15,11 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Transaction;
 
 public class SuccessfulSignUp extends AppCompatActivity {
-    FirebaseAuth fAuth;
-    FirebaseFirestore fStore;
-    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_successful_sign_up);
 
-        fAuth = FirebaseAuth.getInstance();
-        fStore = FirebaseFirestore.getInstance();
-        user = fAuth.getCurrentUser();
-        DocumentReference docRef = fStore.collection("users").document(user.getUid());
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -36,27 +29,5 @@ public class SuccessfulSignUp extends AppCompatActivity {
                 finish();
             }
         },3000);
-
-
-        fStore.runTransaction(new Transaction.Function<Void>() {
-            @Override
-            public Void apply(@NonNull Transaction transaction) throws FirebaseFirestoreException {
-                transaction.update(docRef, "addition quiz score", 0);
-                transaction.update(docRef, "colors quiz score", 0);
-                transaction.update(docRef, "compassion quiz score", 0);
-                transaction.update(docRef, "counting quiz score", 0);
-                transaction.update(docRef, "discipline quiz score", 0);
-                transaction.update(docRef, "doing good quiz score", 0);
-                transaction.update(docRef, "honesty quiz score", 0);
-                transaction.update(docRef, "love quiz score", 0);
-                transaction.update(docRef, "obedience quiz score", 0);
-                transaction.update(docRef, "respect quiz score", 0);
-                transaction.update(docRef, "responsibility quiz score", 0);
-                transaction.update(docRef, "shapes quiz score", 0);
-                transaction.update(docRef, "sociability quiz score", 0);
-                transaction.update(docRef, "subtraction quiz score", 0);
-                return null;
-            }
-        });
     }
 }
